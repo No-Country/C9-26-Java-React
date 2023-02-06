6c5dc292ba2fe882c71cd225d8973f2491d27cf9
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-02-06 20:45:25 -0300

    added Role and @Data anottation

--- backend/src/main/java/com/nocountry/backend/entities/Curso.java
+++ backend/src/main/java/com/nocountry/backend/entities/Curso.java
@@ -1,11 +1,14 @@
 package com.nocountry.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
-@Table(name = "curso")
+@Data
+@AllArgsConstructor
+@Table(name = "COURSE")
 public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,81 +34,9 @@ public class Curso {
     public Curso() {
     }
 
-    public Curso(Integer id, String nombre, String profesor, String dia, int horario, String modalidad, int nivel) {
-        this.id = id;
-        this.nombre = nombre;
-        this.profesor = profesor;
-        this.dia = dia;
-        this.horario = horario;
-        this.modalidad = modalidad;
-        this.nivel = nivel;
-    }
-
     public Curso(Integer id) {
         this.id = id;
     }
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getProfesor() {
-        return profesor;
-    }
-
-    public void setProfesor(String profesor) {
-        this.profesor = profesor;
-    }
-
-    public String getDia() {
-        return dia;
-    }
-
-    public void setDia(String dia) {
-        this.dia = dia;
-    }
-
-    public int getHorario() {
-        return horario;
-    }
-
-    public void setHorario(int horario) {
-        this.horario = horario;
-    }
-
-    public String getModalidad() {
-        return modalidad;
-    }
-
-    public void setModalidad(String modalidad) {
-        this.modalidad = modalidad;
-    }
-
-    public int getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
-    public List<Usuario> getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(List<Usuario> usuario) {
-        this.usuario = usuario;
-    }
 }
