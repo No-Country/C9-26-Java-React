8450eb6e4bba76bee8be512fa0a5bfb783875e6c
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-08 18:52:19 +0100

    perf(frontend): :zap: added barrel to simplify imports

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home/Home';
+export { default as Layout } from '../components/Layout/Layout';
+export { default as Login } from './Login/Login';
+export { default as Profile } from './Profile/Profile';
+export { default as Servicios } from './Servicios/Servicios';
