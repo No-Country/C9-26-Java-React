7d04fdd3b9d51effcc7bf5c76705fcb9d8cf8804
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-08 18:36:15 +0100

    feat(frontend): :sparkles: routing system with react-router

---
+++ frontend/src/config/routes/paths.js
@@ -0,0 +1,7 @@
+export const HOME = '/';
+export const LOGIN = '/login';
+export const REGISTER = '/register';
+export const PROFILE = '/profile';
+export const SERVICES = '/services';
+export const LOGOUT = '/logout';
+export const NOT_FOUND = '*';
