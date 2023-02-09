869f079ed66f3fd8894184bd11e7925b330cf260
Franco Buceta <105551748+francobuceta@users.noreply.github.com>
2023-02-09 11:31:25 -0300

    Home (#11)
    * integracion con rama develop
    
    * Workinng in Home and Login
    
    ---------
    
    Co-authored-by: fdlaurentis <fdlaurentis@gmail.com>

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,58 @@
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import pageOne from '../../assets/images/pageOne.png'
+import pageTwo from '../../assets/images/pageTwo.png'
+import pageThree from '../../assets/images/pageThree.png'
+
+const images = [
+    {
+        id: 1,
+        title: "Imagen One",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
+        imagen: pageOne
+    },
+    {
+        id: 2,
+        title: "Imagen Two",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
+        imagen: pageTwo
+    },
+    {
+        id: 3,
+        title: "Imagen Two",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
+        imagen: pageThree
+    }
+]
+
 const Home = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
-        <div>Home</div>
-    )
+        < Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            interval={2000}
+        >
+            {images.map((image) => (
+                <Carousel.Item key={image.id}>
+                    <img
+                        //style={{ maxHeight: '500px' }}
+                        className="d-block img-fluid w-100"
+                        src={image.imagen}
+                        alt={image.title}
+                    />
+                    {/* <Carousel.Caption>
+                        <h3 style={{color:'black'}}>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption> */}
+                </Carousel.Item>
+            ))}
+        </Carousel >
+    );
 }
 
 export default Home
