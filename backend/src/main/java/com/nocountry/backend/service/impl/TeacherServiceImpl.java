f090997e412ba80a4891b0f7261e9b661a15022c
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-02-11 12:49:36 -0300

    added mapper to teacher

--- backend/src/main/java/com/nocountry/backend/service/impl/TeacherServiceImpl.java
+++ backend/src/main/java/com/nocountry/backend/service/impl/TeacherServiceImpl.java
@@ -2,8 +2,11 @@ package com.nocountry.backend.service.impl;
 
 import com.nocountry.backend.dto.TeacherDto;
 import com.nocountry.backend.mapper.TeacherMapper;
+import com.nocountry.backend.model.Student;
+import com.nocountry.backend.model.Teacher;
 import com.nocountry.backend.repository.ITeacherRepository;
 import com.nocountry.backend.service.ITeacherService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -18,23 +21,26 @@ public class TeacherServiceImpl implements ITeacherService {
     private final TeacherMapper mapper;
 
     @Override
-    public List<TeacherDto> getAll() {
-        return null;
+    public List<TeacherDto> getAll() {return mapper.convertToDtoList(repository.findAll());
     }
 
     @Override
     public Optional<TeacherDto> getById(Long id) {
-        return null;
+        return Optional.ofNullable(mapper.convertToDto(repository.getReferenceById(id)));
     }
 
     @Override
     public TeacherDto create(TeacherDto teacher) {
-        return null;
+        return mapper.convertToDto(repository.save(mapper.convertDtoToEntity(teacher)));
+
     }
 
     @Override
     public TeacherDto update(TeacherDto teacher, Long id) {
-        return null;
+        Teacher updatedTeacher= repository.findById(id).orElseThrow(EntityNotFoundException::new);
+        updatedTeacher.setFirstName(teacher.getFirstName());
+        updatedTeacher.setLastName(teacher.getLastName());
+        return  mapper.convertToDto(repository.save(updatedTeacher));
     }
 
     @Override
