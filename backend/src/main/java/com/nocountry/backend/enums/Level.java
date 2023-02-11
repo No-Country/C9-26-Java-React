640d0fcd11681e1e58f47d22bab9728e3a29fdf1
Francisco Pulido <pulidodev@gmail.com>
2023-02-11 17:47:10 -0300

    add permission enum

--- backend/src/main/java/com/nocountry/backend/enums/Level.java
+++ backend/src/main/java/com/nocountry/backend/enums/Level.java
@@ -1,10 +1,19 @@
 package com.nocountry.backend.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Level {
-    BEGINNER,
-    ELEMENTARY,
-    INTERMEDIATE,
-    UPPER_INTERMEDIATE,
-    ADVANCED,
-    PROFICIENCY
+    A1("A1", "Beginner"),
+    A2("A2", "Elementary"),
+    B1("B1", "Intermediate"),
+    B2("B2", "Upper Intermediate"),
+    C1("C1", "Advanced"),
+    C2("C2", "Proficient");
+
+    private final String levelCode;
+
+    private final String levelDescription;
 }
