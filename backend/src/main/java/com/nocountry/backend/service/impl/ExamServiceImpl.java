795dcb7e04026059cb1ebdac6f61906bff63b1ab
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-02-11 12:56:00 -0300

    added mapper to exam

--- backend/src/main/java/com/nocountry/backend/service/impl/ExamServiceImpl.java
+++ backend/src/main/java/com/nocountry/backend/service/impl/ExamServiceImpl.java
@@ -2,8 +2,11 @@ package com.nocountry.backend.service.impl;
 
 import com.nocountry.backend.dto.ExamDto;
 import com.nocountry.backend.mapper.ExamMapper;
+import com.nocountry.backend.model.Exam;
+import com.nocountry.backend.model.Teacher;
 import com.nocountry.backend.repository.IExamRepository;
 import com.nocountry.backend.service.IExamService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -19,22 +22,32 @@ public class ExamServiceImpl implements IExamService {
 
     @Override
     public List<ExamDto> getAll() {
-        return null;
+        return mapper.convertToDtoList(repository.findAll());
     }
 
     @Override
     public Optional<ExamDto> getById(Long id) {
-        return null;
+        return Optional.ofNullable(mapper.convertToDto(repository.getReferenceById(id)));
     }
 
     @Override
     public ExamDto create(ExamDto exam) {
-        return null;
+        return mapper.convertToDto(repository.save(mapper.convertDtoToEntity(exam)));
     }
 
     @Override
     public ExamDto update(ExamDto exam, Long id) {
-        return null;
+        Exam updatedExam= repository.findById(id).orElseThrow(EntityNotFoundException::new);
+        updatedExam.setName(exam.getName());
+        updatedExam.setExamDate(updatedExam.getExamDate());
+        updatedExam.setExamFee(updatedExam.getExamFee());
+        updatedExam.setStatus(updatedExam.getStatus());
+        updatedExam.setGrammarNote(updatedExam.getGrammarNote());
+        updatedExam.setListeningNote(updatedExam.getListeningNote());
+        updatedExam.setSpeakingNote(updatedExam.getSpeakingNote());
+        updatedExam.setWritingNote(updatedExam.getWritingNote());
+        return  mapper.convertToDto(repository.save(updatedExam));
+
     }
 
     @Override
