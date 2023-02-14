85d2c7ac244f450e0b4ad85a168f755865869703
Franco Buceta <francobuceta95@gmail.com>
2023-02-14 20:59:43 -0300

    add search bar in header (admin view)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,6 +7,8 @@ import { FaWhatsapp, FaGoogle, FaFacebook, FaInstagram, FaYoutube, FaHome, FaUse
 import { useState } from 'react';
 import { Login } from '../../pages';
 import logo from '../../assets/images/Logo.png'
+import { useLocation } from 'react-router-dom';
+import SearchBar from "../SearchBar/SearchBar"
 
 
 function Header() {
@@ -14,9 +16,12 @@ function Header() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    
+
+    const location = useLocation();
+    const path = location.pathname;
+
     return (
-        
+
         <>
             <div className='bg-dark d-flex align-items-center'>
                 <Container className='d-flex gap-5 py-2' fluid>
@@ -29,9 +34,9 @@ function Header() {
                         <Button variant="dark" className='p-1 d-flex justify-content-center align-items-center me-2'>
                             <FaGoogle scale={2} className='m-0' />
                         </Button> <span className='text-white'>greenhills@email.com.ar</span>
-                    </div>                 
-                    <Button                        
-                        style={{ width: '180px', background: '#CB8DF1', color:'#FFFFFF', border:'none' }}
+                    </div>
+                    <Button
+                        style={{ width: '180px', background: '#CB8DF1', color: '#FFFFFF', border: 'none' }}
 
                         className='d-flex justify-content-center align-items-center ms-auto text-uppercase rounded-5 px-5 w-4'
                         onClick={handleShow}
@@ -40,28 +45,42 @@ function Header() {
                     </Button>
                 </Container>
             </div>
-            <Navbar collapseOnSelect expand="lg" style={{ maxHeight:'80px', background:'#212121'}} className='sticky-top shadow-sm'>
+            <Navbar collapseOnSelect expand="lg" style={{ maxHeight: '80px', background: '#212121' }} className='sticky-top shadow-sm'>
                 <Container fluid>
-                    <NavLink to="/"><Navbar.Brand href="#home"><img src={logo} className='img-fluid' style={{width:'150px'}}/></Navbar.Brand></NavLink>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto ">
+                    <NavLink to="/"><Navbar.Brand href="#home"><img src={logo} className='img-fluid' style={{ width: '150px' }} /></Navbar.Brand></NavLink>
 
-                            <NavLink to="/" className='nav-link text-uppercase text-white'>Inicio</NavLink>
-                            <NavLink to="/class" className='nav-link text-uppercase text-white'>Clases</NavLink>
-                            <NavLink to="/exams" className='nav-link text-uppercase text-white'>Examenes</NavLink>
-                            <NavLink to="/services" className='nav-link text-uppercase text-white'>Servicios</NavLink>
-                            <NavLink to="/consultation">
-                                <Button style={{ width: '180px', background: '#CB8DF1', color:'#FFFFFF', border:'none'  }} className='text-uppercase rounded-5 text-light px-5 ms-5'>Consultas</Button>
-                            </NavLink>
-                            
-                        </Nav>
-                    </Navbar.Collapse>
+                    {
+                        path === "/add-student"
+                            ? 
+                            <>
+                                <SearchBar />
+                                <Button style={{ width: '256px', background: '#CB8DF1', color: '#FFFFFF', border: 'none' }} className='text-black fw-bolder rounded-5'>GUARDAR CAMBIOS</Button>
+                            </>
+
+                            :
+                            <>
+                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                                <Navbar.Collapse id="responsive-navbar-nav">
+                                    <Nav className="ms-auto ">
+
+                                        <NavLink to="/" className='nav-link text-uppercase text-white'>Inicio</NavLink>
+                                        <NavLink to="/class" className='nav-link text-uppercase text-white'>Clases</NavLink>
+                                        <NavLink to="/exams" className='nav-link text-uppercase text-white'>Examenes</NavLink>
+                                        <NavLink to="/services" className='nav-link text-uppercase text-white'>Servicios</NavLink>
+                                        <NavLink to="/consultation">
+                                            <Button style={{ width: '180px', background: '#CB8DF1', color: '#FFFFFF', border: 'none' }} className='text-uppercase rounded-5 text-light px-5 ms-5'>Consultas</Button>
+                                        </NavLink>
+
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </>
+                    }
+
                 </Container>
             </Navbar>
-            <Login 
-                show = { show }
-                handleClose = { handleClose }
+            <Login
+                show={show}
+                handleClose={handleClose}
             />
         </>
     );
