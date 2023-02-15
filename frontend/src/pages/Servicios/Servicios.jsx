dd9f2f84385805aaf7a61d04ce90a78c0d63a45e
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-15 14:15:51 +0100

    feat(frontend): :lipstick: improvements to service styles

--- frontend/src/pages/Servicios/Servicios.jsx
+++ frontend/src/pages/Servicios/Servicios.jsx
@@ -9,23 +9,19 @@ import styles from './Servicios.module.css';
 const servicios = [
     {
         title: "Examenes Internacionales",
-        description: "Elegir bien entre la oferta de exámenes internacionales de inglés es clave para conseguir tus objetivos. Por eso, hemos reunido información esencial para ayudarte en este proceso.",
-        image: "https://www.englishinaction.com/wp-content/uploads/2019/10/IELTS-Exam-1.jpg",
+        description: "Elegir bien entre la oferta de exámenes internacionales de inglés es clave para conseguir tus objetivos. Por eso, hemos reunido información esencial para ayudarte en este proceso."
     },
     {
-        title: "Viajes de Estudio",
-        description: "Para estudiar inglés en el exterior existen diferentes propuestas de cursos. Vos elegís el tipo de curso de inglés en el extranjero que te gustaría tomar, según tu necesidad, el país y la ciudad que siempre quisiste conocer!",
-        image: "https://www.englishinaction.com/wp-content/uploads/2019/10/Study-Abroad-1.jpg",
+        title: "Viajes",
+        description: "Para estudiar inglés en el exterior existen diferentes propuestas de cursos. Vos elegís el tipo de curso de inglés en el extranjero que te gustaría tomar, según tu necesidad, el país y la ciudad que siempre quisiste conocer!"
     },
     {
         title: "Traducciones",
-        description: "Ofrecemos los servicios de  traducción e interpretación de calidad y excelenciapara clientes particulares, instituciones o empresas.",
-        image: "https://www.englishinaction.com/wp-content/uploads/2019/10/English-Courses-1.jpg",
+        description: "Ofrecemos los servicios de  traducción e interpretación de calidad y excelenciapara clientes particulares, instituciones o empresas."
     },
     {
         title: "Visas",
-        description: "Te ayudamos a tramitar tu visa de working and holiday son un tipo de visa que te permite viajar a un destino con el fin de trabajar y recorrer el país durante 1 año. Vas a poder ahorrar dinero, conocer gente de todo el mundo, viajar mucho.",
-        image: "https://www.englishinaction.com/wp-content/uploads/2019/10/Exam-Preparation-1.jpg",
+        description: "Te ayudamos a tramitar tu visa de working and holiday son un tipo de visa que te permite viajar a un destino con el fin de trabajar y recorrer el país durante 1 año. Vas a poder ahorrar dinero, conocer gente de todo el mundo, viajar mucho."
     },
 ]
 
@@ -35,7 +31,7 @@ function Servicios() {
             <Container className='w-full'>
                 <Row>
                     <Col className='d-flex flex-column justify-content-center'>
-                        <h1 className="text-center text-light mb-5">Nuestros Servicios</h1>
+                        <h1 className={styles.title}>Nuestros Servicios</h1>
                         <Stack direction="horizontal" gap={3} className='d-flex justify-content-center align-items-stretch flex-wrap'>
                             {servicios.map((servicio, index) => (
                                 <ServiceCard key={index} data={servicio} />
