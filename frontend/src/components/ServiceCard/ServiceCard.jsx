073b0313501546f3ca0ebd5951b20188351ea48c
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-15 14:23:38 +0100

    feat(frontend): :lipstick: improvements to service styles (#20)

--- frontend/src/components/ServiceCard/ServiceCard.jsx
+++ frontend/src/components/ServiceCard/ServiceCard.jsx
@@ -4,15 +4,15 @@ import Button from 'react-bootstrap/Button';
 const ServiceCard = ({ data }) => {
     const { title, description, image } = data
     return (
-        <Card style={{ width: '18rem' }} className='rounded-4' >
+        <Card style={{ width: 'clamp(220px, 80%, 260px)' }} className='rounded-4' >
             <Card.Body className='d-flex flex-column align-items-center'>
                 <Card.Title className='text-center text-uppercase fs-4 my-4'>
                     {title}
                 </Card.Title>
-                <Card.Text>
+                <Card.Text className='text-center'>
                     {description}
                 </Card.Text>
-                <Button variant="dark" className='align-self-end mt-auto rounded-5'>
+                <Button variant="dark" className='mt-auto rounded-5'>
                     Consulte aquí
                 </Button>
             </Card.Body>
