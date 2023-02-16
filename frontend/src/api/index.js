7c73ec4a812c5a4e79ed5a28f2e1ead7e00bbc7e
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-16 17:27:35 +0100

    feat(frontend): :art: add api url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const apiCall = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://nocountry.up.railway.app/api', // process.env.REACT_APP_API_URL,
     timeout: 2000,
 });
