c361af3514d052b8482bdb2cbf8d2d92dac0649b
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-18 18:02:33 +0100

    fix(frontend): :adhesive_bandage: improvements to Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import pageOne from '../../assets/images/pageOne.jpg'
-import pageTwo from '../../assets/images/pageTwo.jpg'
-import pageThree from '../../assets/images/pageThree.jpg'
+import { Carousel, Button, Row, Col } from 'react-bootstrap';
+import discount from '../../assets/images/slider/discount.svg'
+
+import pageOne from '../../assets/images/slider/pageOne.png'
+import pageTwo from '../../assets/images/slider/pageTwo.png'
+import pageThree from '../../assets/images/slider/pageThree.png'
+
+import liberty from '../../assets/images/slider/liberty.svg'
+import bigben from '../../assets/images/slider/big-ben.svg'
+
 
 const images = [
     {
         id: 1,
-        title: "Imagen One",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
-        imagen: pageOne
+        title: "Cursos de Inglés",
+        description: "Para alumnos de todos los niveles y edades.",
+        imagen: pageOne,
+        backgrouds: [bigben, liberty]
     },
     {
         id: 2,
-        title: "Imagen Two",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
-        imagen: pageTwo
+        title: `Clases in-company`,
+        description: "Llevamos las clases a tu empresa.",
+        imagen: pageTwo,
+        backgrouds: []
     },
     {
         id: 3,
-        title: "Imagen Two",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum.",
-        imagen: pageThree
+        title: "Clases personalizadas",
+        description: "Clases de conversación con fines específicos, presenciales y online.",
+        imagen: pageThree,
+        backgrouds: []
     }
 ]
 
@@ -32,21 +41,40 @@ const Home = () => {
         setIndex(selectedIndex);
     };
     return (
-        < Carousel
-            activeIndex={index}
-            onSelect={handleSelect}
-            interval={2000}
-        >
-            {images.map((image) => (
-                <Carousel.Item key={image.id}>
-                    <img                        
-                        className="d-block img-fluid w-100"
-                        src={image.imagen}
-                        alt={image.title}
-                    />                  
-                </Carousel.Item>
-            ))}
-        </Carousel >
+        <main className='background d-flex grow'>
+            < Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                // interval={2000}
+                className='d-flex grow overflow-hidden w-100'
+            >
+                {images.map((image) => (
+                    <Carousel.Item key={image.id} className='grow relative '>
+
+                        <Row className='d-flex flex-md-row-reverse relative h-100'>
+
+                            <Col md={6} className='d-flex justify-content-center align-items-center'>
+                                <div className='discount'>
+                                    <img src={discount} alt={image.title} />
+                                </div>
+                            </Col>
+                            <Col md={6} className='d-flex justify-content-center align-items-end px-2 w-70 position-relative'>
+                                <img className='w-xl-100 position-absolute bottom-0' src={image.imagen} alt={image.title} />
+                            </Col>
+
+                        </Row>
+                        <Carousel.Caption className='relative' >
+
+                            <h3 className='text-uppercase fs-1' style={{ fontWeight: 'bolder', textShadow: '0px 0px 10px black' }}>{image.title}</h3>
+                            <p style={{ textShadow: '0px 0px 10px black' }}>{image.description}</p>
+                            <Button className='mb-4 fs-3 button_home rounded-4'>Inscripción Abierta <br /> 2023</Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+
+
+            </Carousel >
+        </main >
     );
 }
 
