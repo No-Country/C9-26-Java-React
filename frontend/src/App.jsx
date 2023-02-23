8b59a84b55327497930529c69fcce888afebaec8
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-23 11:14:40 +0100

    updated navigation paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HOME, CAMPUS, PROFILE, CLASS, EXAMS, SERVICES, CONSULTATION, STUDENT, ADD_STUDENT } from './config/routes/paths'
-import { PrivateRoute, PublicRoute } from './components'
+import { HOME, CAMPUS, PRIVATE, CLASS, EXAMS, SERVICES, CONSULTATION, STUDENT, ADD_STUDENT, PROGRAM, MULTIMEDIA, CHAT, EVENTS, TASKS } from './config/routes/paths'
+import { PrivateRoute, PublicRoute, Program, Multimedia, Chat, Events, Tasks } from './components'
 import { Home, Campus, Profile, Clases, Examenes, Servicios, Consultas, Alumno } from './pages'
 
 
@@ -9,19 +9,24 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<PublicRoute />}>
-          <Route path={HOME} element={<Home />} />
-          {/* <Route path={LOGIN} element={<Login />} /> */}
+        <Route path={HOME} element={<PublicRoute />}>
+          <Route index element={<Home />} />
           <Route path={CLASS} element={<Clases />} />
           <Route path={EXAMS} element={<Examenes />} />
           <Route path={SERVICES} element={<Servicios />} />
           <Route path={CONSULTATION} element={<Consultas />} />
-          <Route path={STUDENT} element={<Alumno />} />
-          <Route path={ADD_STUDENT} element={<Alumno />} />
-          <Route path={CAMPUS} element={<Campus />} />
         </Route>
-        <Route path={PROFILE} element={<PrivateRoute />}>
+        <Route path={PRIVATE} element={<PrivateRoute />}>
           <Route index element={<Profile />} />
+          <Route path={ADD_STUDENT} element={<Alumno />} />
+          <Route path={STUDENT} element={<Alumno />} />
+          <Route path={CAMPUS} element={<Campus />}>
+            <Route path={PROGRAM} element={<Program />} />
+            <Route path={MULTIMEDIA} element={<Multimedia />} />
+            <Route path={CHAT} element={<Chat />} />
+            <Route path={EVENTS} element={<Events />} />
+            <Route path={TASKS} element={<Tasks />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
