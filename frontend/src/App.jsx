eac066151e5f10d12e7bfa09bb95468f73726edb
Alberto <agomezjuan@hotmail.com>
2023-02-25 14:16:01 +0100

    include path to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HOME, CAMPUS, PRIVATE, CLASS, EXAMS, SERVICES, CONSULTATION, STUDENT, ADD_STUDENT, PROGRAM, MULTIMEDIA, CHAT, EVENTS, TASKS } from './config/routes/paths'
+import { HOME, CAMPUS, PRIVATE, CLASS, EXAMS, SERVICES, CONSULTATION, STUDENT, ADD_STUDENT, PROGRAM, MULTIMEDIA, CHAT, EVENTS, TASKS, LOGIN } from './config/routes/paths'
 import { PrivateRoute, PublicRoute, Program, Multimedia, Chat, Events, Tasks } from './components'
-import { Home, Campus, Profile, Clases, Examenes, Servicios, Consultas, Alumno } from './pages'
+import { Home, Campus, Profile, Clases, Examenes, Servicios, Consultas, Alumno, Login } from './pages'
 
 
 
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path={HOME} element={<PublicRoute />}>
           <Route index element={<Home />} />
+          <Route path={LOGIN} element={<Login />} />
           <Route path={CLASS} element={<Clases />} />
           <Route path={EXAMS} element={<Examenes />} />
           <Route path={SERVICES} element={<Servicios />} />
@@ -21,6 +22,7 @@ function App() {
           <Route path={ADD_STUDENT} element={<Alumno />} />
           <Route path={STUDENT} element={<Alumno />} />
           <Route path={CAMPUS} element={<Campus />}>
+            <Route index element={<Program />} />
             <Route path={PROGRAM} element={<Program />} />
             <Route path={MULTIMEDIA} element={<Multimedia />} />
             <Route path={CHAT} element={<Chat />} />
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -9,11 +9,12 @@ import { login } from '../../store/actions/userActions';
 import { useEffect, useState } from 'react';
 import { useAuth, useLoading, useUserInfo } from '../../hooks/userHooks';
 import { useNavigate } from 'react-router-dom';
-import { studentInfo } from '../../store/actions/userActions';
+import { CAMPUS, HOME } from '../../config/routes/paths';
 
-const Login = ({ show, handleClose }) => {
-  const { isAuthenticated, token } = useAuth();
-  const userInfo = useUserInfo();
+import Home from '../Home/Home';
+
+const Login = () => {
+  const { isAuthenticated } = useAuth();
   const isLoading = useLoading();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,47 +27,52 @@ const Login = ({ show, handleClose }) => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate('/private/campus')
-      handleClose()
+      navigate(CAMPUS)
     }
-  }, [])
+  }, [isAuthenticated])
 
+  const handleClose = () => {
+    navigate(HOME)
+  }
 
   const onSubmit = data => {
     dispatch(login(data))
+
   };
 
 
   return (
-    <div>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        className={style.modal}
-        centered='true'
-      >
-        <Modal.Header style={{ background: '#000000', border: 'none', color: '#FFFFFF' }}>
-          <h2>Por favor ingresa tus datos</h2>
-          <BsFillXCircleFill onClick={handleClose} style={{ cursor: 'pointer' }} />
-        </Modal.Header>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Modal.Body style={{ background: '#000000', border: 'none' }}>
-            <div className={style.body}>
-              <input type="email" placeholder='Username' className={style.input} {...register('username', { required: true })} />
-              {errors.username && <span className={style.errors}>Se requiere un nombre de usuario</span>}
-              <div className={style.input}><input type={visible ? "text" : "password"} placeholder='Password' {...register('password', { required: true })} /> {visible ? <AiFillEye onClick={() => setVisible(!visible)} /> : <AiFillEyeInvisible onClick={() => setVisible(!visible)} />}</div>
-              {errors.password && <span className={style.errors}>Se requiere la contraseña</span>}
+    <>
+      <Home />
+      <div>
+        <Modal
+          show={true}
+          className={style.modal}
+          centered='true'
+        >
+          <Modal.Header style={{ background: '#000000', border: 'none', color: '#FFFFFF' }}>
+            <h2>Por favor ingresa tus datos</h2>
+            <BsFillXCircleFill onClick={handleClose} style={{ cursor: 'pointer' }} />
+          </Modal.Header>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Modal.Body style={{ background: '#000000', border: 'none' }}>
+              <div className={style.body}>
+                <input type="email" placeholder='Username' className={style.input} {...register('username', { required: true })} />
+                {errors.username && <span className={style.errors}>Se requiere un nombre de usuario</span>}
+                <div className={style.input}><input type={visible ? "text" : "password"} placeholder='Password' {...register('password', { required: true })} /> {visible ? <AiFillEye onClick={() => setVisible(!visible)} /> : <AiFillEyeInvisible onClick={() => setVisible(!visible)} />}</div>
+                {errors.password && <span className={style.errors}>Se requiere la contraseña</span>}
 
-            </div>
-          </Modal.Body>
-          <Modal.Footer style={{ background: '#000000', border: 'none', display: 'flex', justifyContent: 'center' }}>
-            <button className={style.button}>
-              {isLoading ? 'VERIFICANDO...' : 'INGRESAR'}
-            </button>
-          </Modal.Footer>
-        </form>
-      </Modal>
-    </div >
+              </div>
+            </Modal.Body>
+            <Modal.Footer style={{ background: '#000000', border: 'none', display: 'flex', justifyContent: 'center' }}>
+              <button className={style.button}>
+                {isLoading ? 'VERIFICANDO...' : 'INGRESAR'}
+              </button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+      </div >
+    </>
   )
 }
 
