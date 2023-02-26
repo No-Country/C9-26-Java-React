904d73500563e2e98c4132e27ab76217b9469e14
Francisco Pulido <pulidodev@gmail.com>
2023-02-26 10:45:36 -0300

    test

--- backend/src/main/java/com/nocountry/backend/config/AppConfig.java
+++ backend/src/main/java/com/nocountry/backend/config/AppConfig.java
@@ -52,8 +52,7 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "https://bright-english.vercel.app:*",
-                        "http://127.0.0.1:*")
+                .allowedOrigins("https://bright-english.vercel.app", "http://127.0.0.1:5173", "http://localhost:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
