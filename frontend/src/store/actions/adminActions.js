f551972c3ca092b7a806e9873c4011afb1f8af09
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-27 08:36:39 +0100

    more admin actions

--- frontend/src/store/actions/adminActions.js
+++ frontend/src/store/actions/adminActions.js
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { apiCall } from "../../api";
-import { useSelector } from "react-redux";
+import { useAuth } from "../../hooks/userHooks";
 
+// get all students
 export const getStudents = createAsyncThunk(
     "admin/getStudents",
     async ({ rejectWithValue }) => {
-        const token = useSelector((state) => state.user.token);
+        const { token } = useAuth()
         try {
             const response = await apiCall.get("/students/admin/all", {
                 headers: {
@@ -18,3 +19,76 @@ export const getStudents = createAsyncThunk(
         }
     }
 );
+
+// get student by id
+export const getStudentById = createAsyncThunk(
+    "admin/getStudentById",
+    async (id, { rejectWithValue }) => {
+        try {
+            const { token } = useAuth()
+            const response = await apiCall.get(`/students/admin/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
+// get all courses
+export const getCourses = createAsyncThunk(
+    "admin/getCourses",
+    async ({ rejectWithValue }) => {
+        try {
+            const { token } = useAuth()
+            const response = await apiCall.get("/courses/all", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+
+    }
+)
+
+// get all teachers
+export const getTeachers = createAsyncThunk(
+    "admin/getTeachers",
+    async ({ rejectWithValue }) => {
+        try {
+            const { token } = useAuth()
+            const response = await apiCall.get("/teachers/all", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+)
+
+// get teacher by id
+export const getTeacherById = createAsyncThunk(
+    "admin/getTeacherById",
+    async (id, { rejectWithValue }) => {
+        try {
+            const { token } = useAuth()
+            const response = await apiCall.get(`/teachers/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+)
