4d12fa8ca9609b898116a2830fc69a51e876213a
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-28 01:34:52 +0100

    not found page styles

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <main className="background not_found">
+            <h1>404</h1>
+            <h2>El recurso que buscabas no ya no existe</h2>
+            <Link to="/">Volver al inicio</Link>
+        </main>
     )
 }
 export default NotFound
