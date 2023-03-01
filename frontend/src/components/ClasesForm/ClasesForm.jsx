5ecb394683765f58f08577486bd314acf829e6da
Franco Buceta <francobuceta95@gmail.com>
2023-03-01 11:40:43 -0300

    add react formspree

--- frontend/src/components/ClasesForm/ClasesForm.jsx
+++ frontend/src/components/ClasesForm/ClasesForm.jsx
@@ -1,5 +1,4 @@
-import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useForm } from '@formspree/react';
 import { useNavigate } from 'react-router-dom';
 import { HOME } from "../../config/routes/paths";
 import Modal from 'react-bootstrap/Modal';
@@ -14,27 +13,8 @@ const ClasesForm = () => {
         "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucumán"];
 
     //Funcionalidad del formulario
-    const { register, formState: { errors } } = useForm();
-    const [sentForm, setSendForm] = useState(false);
+    const [state, handleSubmit] = useForm("mvonrkld");
     const navigate = useNavigate();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const form = new FormData(e.target);
-        /* const data =  */await fetch(e.target.action, {
-            method: e.target.method,
-            mode: 'no-cors',
-            body: form,
-            header: {
-                "Accept": "application/json",
-            }
-        })
-            .then(() => {
-                console.log("enviado");
-                setSendForm(true);
-            })
-            .catch(() => console.log("no pudo enviarse"));
-    }
 
     const handleClose = () => {
         navigate(HOME);
@@ -49,49 +29,42 @@ const ClasesForm = () => {
                     y nos pondremos en contacto para asesorarte y completar la inscripción
                 </p>
 
-                <form action="https://formspree.io/f/mvonrkld" method="POST" onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <div className={styles.input_container}>
-                        <input type="text" placeholder="Nombre y apellido" {...register("nombre", { required: "Campo requerido" })} />
-                        <p className="fw-bold text-black">{errors.nombre?.message}</p>
+                        <input type="text" name="nombre" placeholder="Nombre y apellido" required />
                     </div>
 
                     <div className={styles.input_container}>
-                        <input type="number" name="tel" id="" placeholder="Tel. +54 9" {...register("tel", { required: "Campo requerido" })} />
-                        <p className="fw-bold text-black">{errors.tel?.message}</p>
+                        <input type="number" name="tel" id="" placeholder="Tel. +54 9" required />
                     </div>
 
                     <div className={styles.input_container}>
-                        <input type="email" name="" id="" placeholder="ingresa@tuemail.com" {...register("mail", { required: "Campo requerido" })} />
-                        <p className="fw-bold text-black">{errors.mail?.message}</p>
+                        <input type="email" name="email" id="" placeholder="ingresa@tuemail.com" required />
                     </div>
 
                     <div className={styles.input_container}>
-                        <input type="text" placeholder="Fecha de nacimiento" max="2018-12-25" {...register("nacimiento", { required: "Campo requerido" })} onFocus={(e) => (e.target.type = "date")} />
-                        <p className="fw-bold text-black">{errors.nacimiento?.message}</p>
+                        <input type="text" name="fecha" placeholder="Fecha de nacimiento" max="2018-12-25" onFocus={(e) => (e.target.type = "date")} required />
                     </div>
 
                     <div className={styles.form_select}>
-                        <select name="select" id="" defaultValue="" {...register("provincia", { required: "Campo requerido" })}>
+                        <select name="select" id="" defaultValue="" required >
                             <option value="" disabled>Provincia</option>
                             {states.map(elem => <option key={elem} value={elem} className={styles.options_bg}>{elem}</option>)}
                         </select>
-                        <p className="fw-bold text-black">{errors.provincia?.message}</p>
                     </div>
 
                     <div className={styles.form_select}>
-                        <select name="nivel" id="" defaultValue="" {...register("nivel", { required: "Campo requerido" })}>
+                        <select name="nivel" id="" defaultValue="" required >
                             <option value="" disabled>Nivel</option>
                             {level.map(elem => <option key={elem} value={elem} className={styles.options_bg}>{elem}</option>)}
                         </select>
-                        <p className="fw-bold text-black">{errors.nivel?.message}</p>
                     </div>
 
                     <div className={styles.form_select}>
-                        <select name="curso" id="" defaultValue="" {...register("curso", { required: "Campo requerido" })}>
+                        <select name="curso" id="" defaultValue="" required >
                             <option value="" disabled>Curso de mi interés</option>
                             {course.map(elem => <option key={elem} value={elem} className={styles.options_bg}>{elem}</option>)}
                         </select>
-                        <p className="fw-bold text-black">{errors.curso?.message}</p>
                     </div>
 
                     <div className={styles.submit_container}>
@@ -101,15 +74,15 @@ const ClasesForm = () => {
                 </form>
 
                 {
-                    sentForm &&
+                    state.succeeded &&
                     <Modal className={styles.modal_container} show={true} centered="true">
-                            <Modal.Body className={styles.modal}>
-                                <p className="text-white text-center fw-bold">¡Gracias por tu consulta!
-                                    Verificá tu casilla de correo para ver confirmación de tu pedido. En breve nos 
-                                    comunicaremos para completar la solicitud de inscripción.
-                                </p>
-                                <button className={styles.modal_button} onClick={handleClose}>CERRAR</button>
-                            </Modal.Body>
+                        <Modal.Body className={styles.modal}>
+                            <p className="text-white text-center fw-bold">¡Gracias por tu consulta!
+                                Verificá tu casilla de correo para ver confirmación de tu pedido. En breve nos
+                                comunicaremos para completar la solicitud de inscripción.
+                            </p>
+                            <button className={styles.modal_button} onClick={handleClose}>CERRAR</button>
+                        </Modal.Body>
                     </Modal>
                 }
             </div>
