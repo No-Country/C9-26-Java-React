3ff9f9b0f8569f02838c733760ca3349c0026626
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-03-01 18:24:14 -0300

    added endpoint updateImageProfile

---
+++ backend/src/main/java/com/nocountry/backend/exception/ApiRequestException.java
@@ -0,0 +1,11 @@
+package com.nocountry.backend.exception;
+
+public class ApiRequestException extends RuntimeException{
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
