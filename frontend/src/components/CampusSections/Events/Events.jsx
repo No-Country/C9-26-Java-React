18086d41fb281a3f64ed39a39a23e4499516209c
Alberto <agomezjuan@hotmail.com>
2023-03-01 12:28:05 +0100

    calendar

--- frontend/src/components/CampusSections/Events/Events.jsx
+++ frontend/src/components/CampusSections/Events/Events.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import './Events.css'
+import './base.css'
 
-const Events = () => {
-    return (
-        <div>Events</div>
-    )
+const events = [
+  { title: 'Inicio inscripciones', start: new Date('2023-03-1') },
+  { title: 'Cierre inscripciones', start: new Date('2023-03-20') },
+  { title: 'Seminario c/ Laura Lewin', start: new Date('2023-03-25') },
+  { title: 'Reunion nuevos alumnos', start: new Date('2023-03-30') },
+]
+
+function Events() {
+  return (
+    <div className='events'>
+      <h3 className='text-white text-center fs-5 pb-3'>Calendario de eventos, exámenes y actividades.</h3>
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView='dayGridMonth'
+          locale='es'
+          weekends={false}
+          events={events}
+          eventContent={renderEventContent}
+          displayEventTime={false}
+          buttonText={{
+            today: 'Hoy',
+            month: 'Mes',
+            week: 'Semana',
+            day: 'Día',
+            list: 'Lista'
+          }}
+          headerToolbar={{
+            left: 'today prev,next',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,dayGridDay'
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+// a custom render function
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
 }
 
 export default Events
