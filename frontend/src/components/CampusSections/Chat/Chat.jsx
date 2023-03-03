345a6ae5926f5a04c127b08c2d22b66e50a6b141
Fdlaurentis <101360874+Fdlaurentis@users.noreply.github.com>
2023-03-02 21:38:58 -0500

    Trabajo del chat (#48)

--- frontend/src/components/CampusSections/Chat/Chat.jsx
+++ frontend/src/components/CampusSections/Chat/Chat.jsx
@@ -2,15 +2,25 @@ import React, { useState } from 'react'
 import style from './Chat.module.css'
 import { BsSearch } from "react-icons/bs";
 import { AiOutlineSend } from "react-icons/ai";
-
+import contactos from './Contactos.json'
 
 const Chat = () => {
 
     const [sms, setSms] = useState('')
-
-    const handleSubmit = (e)=>{
+    const [msj, setMsj] = useState('')
+    const [posicion, setPosicion] = useState(0)
+    const [bolean, setBolean] = useState(false)
+    const handleSubmit = (e) => {
         e.preventDefault()
-        alert('haz hecho un click')
+        setMsj(sms)
+        setSms('')
+        setBolean(true)
+    }
+
+    const handleClick = (index) => {
+        setPosicion(index)
+        setBolean(false)
+        setMsj('')
     }
 
     return (
@@ -26,6 +36,19 @@ const Chat = () => {
             <div className={style.divBody}>
                 <div className={style.dicContacto}>
                     <h5 style={{ borderBottom: '1px solid #FFFFFF', padding: '10px' }}>CONTACTOS</h5>
+                    <ul className={style.liConatct}>
+                        {contactos.map((contacto, index) => (
+                            <div className={style.divLiConatct} key={index}>
+                                <img
+                                    style={{ width: '30px', borderRadius: '50%' }}
+                                    src={contacto.image}
+                                />
+                                <li
+                                    onClick={() => handleClick(contactos.indexOf(contacto))}
+                                    className={posicion == contactos.indexOf(contacto) ? style.activo : null}>{contacto.name}</li>
+                            </div>
+                        ))}
+                    </ul>
                 </div>
                 <div className={style.divSms}>
                     <h5 style={{ borderBottom: '1px solid #FFFFFF', padding: '10px' }}>MENSAJES</h5>
@@ -34,13 +57,13 @@ const Chat = () => {
                     </div>
                     <form onSubmit={handleSubmit}>
                         <div className={style.divForm}>
-                            <label>{}</label>
+                            <span className={bolean && style.divFormSpan}>{msj}</span>
                         </div>
                         <div className={style.divSmsInput}>
-                            <input 
-                                className = {style.inputSms}
-                                onChange = { e=> setSms(e.target.value)}
-                                value= {sms}
+                            <input
+                                className={style.inputSms}
+                                onChange={e => setSms(e.target.value)}
+                                value={sms}
                             />
                             <button className={style.btnSms}>
                                 <AiOutlineSend />
