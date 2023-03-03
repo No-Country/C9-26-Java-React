e3dea663cec7a9caf2e48ae670dd06d0f30a4a36
fdlaurentis <fdlaurentis@gmail.com>
2023-03-02 21:38:29 -0500

    Trabajo del chat
    Realice el renderizado para el chat y algunos estilos para el Talks

--- frontend/src/components/CampusSections/Tasks/Tasks.jsx
+++ frontend/src/components/CampusSections/Tasks/Tasks.jsx
@@ -5,33 +5,33 @@ import tasks from './Tasks.json'
 const Tasks = () => {
 
     const [posicion, setPosicion] = useState(0)
-
     const handleClick = (index) => {
         setPosicion(index)
-
     }
     return (
-        <div className={style.main}>
-            <div style={{ textAlign: 'center' }}>
-                <h1>TAREA</h1>
-                <ul className={style.taskUl}>
-                    {tasks.map((task, index) => (
-                        <li
-                            key={index}
-                            onClick={() => handleClick(tasks.indexOf(task))}
-                            className={posicion == tasks.indexOf(task) ? style.activo : null}
-                        ><h5>{task.title}</h5></li>
-                    ))}
-                </ul>
-            </div>
-            <h4 className={style.taskH4}>Haciendo click sobre las tareas podrás acceder al correspondiente Google Classroom para ver las consignas y resultados:</h4>
-            <div className={style.content}>
-                <h5>{tasks[posicion].title}</h5>
-                <ul style={{ border: 'none', listStyle: 'none' }}>
-                    {tasks[posicion].contents.map((content, index) => (
-                        <li key={index}>{content}</li>
-                    ))}
-                </ul>
+        <div className={style.contTalsk}>
+            <h1 style={{textAlign:'center'}}>TAREA</h1>
+            <div className={style.contSesion}>
+                <div className={style.divUnidad}>                   
+                        {tasks.map((task, index) => (
+                            <span
+                                key={index}
+                                onClick={() => handleClick(tasks.indexOf(task))}
+                                className={posicion == tasks.indexOf(task) ? style.activo : null}
+                            >{task.title}</span>
+                        ))}                  
+                </div>
+                <div className={style.divCont}>
+                    <h4 style={{textAlign:'center'}}>Haciendo click sobre las tareas podrás acceder al correspondiente Google Classroom para ver las consignas y resultados:</h4>
+                    <div style={{padding:'1rem'}}>
+                        <h5>{tasks[posicion].title}</h5>
+                        <ul>
+                            {tasks[posicion].contents.map((content, index) => (
+                                <li key={index}>{content}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
