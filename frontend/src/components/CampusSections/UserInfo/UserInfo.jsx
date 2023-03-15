577dd4039575318242d3702d95c028661e91ae9c
Franco Buceta <105551748+francobuceta@users.noreply.github.com>
2023-03-15 15:55:31 -0300

    fix url image profile (#50)

--- frontend/src/components/CampusSections/UserInfo/UserInfo.jsx
+++ frontend/src/components/CampusSections/UserInfo/UserInfo.jsx
@@ -3,15 +3,21 @@ import { useUserInfo } from '../../../hooks/userHooks';
 import { STUDENT } from '../../../config/routes/paths';
 import styles from './UserInfo.module.css';
 import LevelProgress from './LevelProgress';
+import { useSelector } from "react-redux";
 
 const UserInfo = () => {
     const { firstName, level } = useUserInfo();
+
+    const info = useSelector(state => state.user.info);
+
+    let imageUrlModified = info.imageResource.urlSecure.replace("http", "https");
     let avatar = 'https://www.w3schools.com/howto/img_avatar.png';
+    
     return (
         <>
             <div className={styles.user}>
                 <div className={styles.user__avatar}>
-                    <img src={avatar} alt="avatar" />
+                    <img src={imageUrlModified || avatar} alt="avatar" />
                 </div>
                 <div className={styles.user__name}>
                     <h2>¡Hola, {firstName || 'usuario'}!</h2>
